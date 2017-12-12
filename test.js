f0f6ba354839e41b3f54403aa538c56e4646e73b
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-12-12 14:21:11 +0100

    included .git into dirs count

--- test.js
+++ test.js
@@ -7,9 +7,12 @@ tape('names only', function (t) {
   wishlist('.', function (err, list) {
     if (err) t.end(err)
 
-    t.is(list.dirs.length, 1, 'should have detected 1 dir')
-    t.is(list.dirs[0], 'node_modules', 'should have detected node_modules')
-    t.ok(list.files.indexOf('test.js') !== -1, 'test.js should be among files')
+    t.is(list.dirs.length, 2, 
+	 'should have detected 2 dirs')
+    t.ok(list.dirs.indexOf('node_modules') !== -1, 
+	 'should have detected node_modules')
+    t.ok(list.files.indexOf('test.js') !== -1, 
+	 'test.js should be among files')
 
     t.end()
   })
@@ -23,8 +26,10 @@ tape('fullpaths', function (t) {
   wishlist('.', { full: true }, function (err, list) {
     if (err) t.end(err)
 
-    t.is(list.dirs.length, 1, 'should have detected 1 dir')
-    t.is(list.dirs[0], expecting, 'should have detected ./node_modules')
+    t.is(list.dirs.length, 2, 
+	 'should have detected 2 dirs')
+    t.ok(list.dirs.indexOf(expecting) !== -1,
+	 'should have detected ./node_modules')
 
     t.end()
   })
